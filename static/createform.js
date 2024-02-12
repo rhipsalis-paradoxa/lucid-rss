272e461f3ee919c81564de93cef49855be19b6e4
koi <koi@atelier.my.domain>
2024-02-12 12:47:01 -0600

    connect xpath parser to frontend

---
+++ static/createform.js
@@ -0,0 +1,12 @@
+// Load a preview of the RSS feed given by the user's parser template.
+async function loadPreview() {
+    const form = document.querySelector('#input-template');
+    const formData = new FormData(form);
+
+    const response = await fetch(window.location.href + "/preview", {
+        method: "POST",
+        body: formData,
+    });
+
+    console.log(await response.text());
+}
